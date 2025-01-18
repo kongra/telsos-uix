3d44f681c77412f7f191b1b3dcfaa6ad1098fcc7
Konrad Grzanek <kongra@gmail.com>
2025-01-18 22:40:16 +0100

    Added react strict mode and telsos.lib

--- src/cljs/telsos/ui/core.cljs
+++ src/cljs/telsos/ui/core.cljs
@@ -1,16 +1,26 @@
 (ns telsos.ui.core
+  (:require-macros
+   [telsos.lib.assertions :refer [the]])
   (:require
+   ["react" :as react]
    [uix.core :refer [$ defui]]
    [uix.dom]))
 
-(defui ui-app []
-  ($ :h1 "Hello, UIx!"))
+(defui ^:private ui-vspace [{:keys [height]}]
+  (the nat-int? height)
+  ($ :div.ui-vspace {:style {:height height}}))
+
+(defui ^:private ui-app []
+  (println "ui-app")
+  ($ :div
+     ($ :h1 "Hello, UIx!")
+     ($ ui-vspace {:height 58})))
 
 (defonce ^:private root
   (uix.dom/create-root (js/document.getElementById "root")))
 
-(defn render-root []
-  (uix.dom/render-root ($ ui-app) root))
+(defn ^:private render-root []
+  (uix.dom/render-root ($ react/StrictMode ($ ui-app)) root))
 
 (defn ^:export init []
   (render-root))
